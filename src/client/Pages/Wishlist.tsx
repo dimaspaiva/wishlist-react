365a07b6ed6b792d0c99a9b542e259ed78462712
dimas <dimasalpaiva@gmail.com>
2021-09-17 01:50:04 -0300

    feat(links): add links between pages

--- src/client/Pages/Wishlist.tsx
+++ src/client/Pages/Wishlist.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from "../components/Header";
 
 const HomePage = () => {
   return (
     <div>
+      <Header />
       <h1>Wishlist</h1>
     </div>
   );
--- src/client/components/Header/index.tsx
+++ src/client/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { MapPin, Phone, Heart, Search } from "react-feather";
+import { Link } from "react-router-dom";
 
 import {
   HeaderBackground,
@@ -13,9 +14,11 @@ const Header = () => {
   return (
     <HeaderBackground>
       <HeaderContainer className="clearfix">
-        <div className="header__logo-container">
-          <h1 className="header__logo-title">MagaNets</h1>
-        </div>
+        <Link to="/">
+          <div className="header__logo-container">
+            <h1 className="header__logo-title">MagaNets</h1>
+          </div>
+        </Link>
 
         <div className="header__action-container">
           <div className="header__actions-list clearfix">
@@ -31,12 +34,14 @@ const Header = () => {
               </IconContainer>
               <h3 className="action__text">Central de atendimento</h3>
             </div>
-            <div className="header__action action--last">
-              <IconContainer>
-                <Heart fill="#eaeaea" />
-              </IconContainer>
-              <h3 className="action__text">Lista de desejos</h3>
-            </div>
+            <Link to="/wishlist">
+              <div className="header__action action--last">
+                <IconContainer>
+                  <Heart fill="#eaeaea" />
+                </IconContainer>
+                <h3 className="action__text">Lista de desejos</h3>
+              </div>
+            </Link>
           </div>
           <div className="header__search-container clearfix">
             <SearchIconContainer>
