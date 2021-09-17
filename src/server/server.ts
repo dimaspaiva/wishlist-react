17aa65a1173de317078d0db85b039b6966acc07f
dimas <dimasalpaiva@gmail.com>
2021-09-17 00:07:31 -0300

    chore(server): name adjustments

--- src/server/server.ts
+++ src/server/server.ts
@@ -5,26 +5,22 @@ import nunjucks from "nunjucks";
 const app = express();
 
 const pagesPath = join(__dirname, "pages");
-const componentsPath = join(__dirname, "components");
 
 app.use(express.static(join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.set("view engine", "njk");
-nunjucks.configure([pagesPath, componentsPath], {
+nunjucks.configure([pagesPath], {
   autoescape: true,
   express: app,
 });
 
 app.get("/", (req, res) => {
-  res.json({
-    message: "sending something",
+  res.render("template", {
+    title: "magalu",
+    message: "Running nunjucks!",
   });
-});
-
-app.get("/search", (req, res) => {
-  res.render("index", { title: "magalu", message: "Running nunjucks!" });
 });
 
 export default app;
