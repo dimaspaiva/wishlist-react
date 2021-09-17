40f8889f5f275f6de30fd4d7538d75170e81bca5
dimas <dimasalpaiva@gmail.com>
2021-09-17 01:26:37 -0300

    chore(routes): fix names

--- src/client/Pages/Wishlist.tsx
+++ src/client/Pages/Wishlist.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
     <div>
-      <h1>HOME :D</h1>
-      <Link to="/search">Search ???</Link>
+      <h1>Wishlist</h1>
     </div>
   );
 };
--- src/client/routes.tsx
+++ src/client/routes.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import HomePage from "./Pages/Home";
-import SearchPage from "./Pages/Search";
+import Wishlist from "./Pages/Wishlist";
+import Home from "./Pages/Home";
 
 const Routes = () => {
   return (
     <Router>
-      <Route path="/" component={HomePage} exact />
-      <Route path="/search" component={SearchPage} />
+      <Route path="/" component={Home} exact />
+      <Route path="/wishlist" component={Wishlist} />
     </Router>
   );
 };
